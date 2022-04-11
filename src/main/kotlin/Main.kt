3f27ead220a946216fa1955bf063eee47b7efc32
mani869081 <mani869081@gamil.com>
2022-04-11 16:20:58 +0530

    hi

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,21 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    val  write : Char
-    write = 'e'
-    println("$write")
+    val a : Int = 100
+    val d : Double = 10.00
+    val f : Float = 1000.00f
+    val l : Long = 10000000001432561
+    val s : Short = 24
 
+    println("The value of integer is $a" )
+    println("The value of integer is $d" )
+    println("The value of integer is $f" )
+    println("The value of integer is $l" )
+    println("The value of integer is $s" )
 
     println("Program arguments: ${args.joinToString()}")
+
+    var write : Boolean
+    write = false
+    println("$write")
 }
